6b702bfee4073fe8795af26310a3ddc4b6200510
JanithSandaru <janithsandaru999@gmail.com>
2022-09-19 11:24:30 +0530

    added

---
+++ SpringBoot_POS/src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppInitializer.class);
+    }
+}
